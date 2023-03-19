a8084ee1715a2d22630fe9e5ab4a8c946d7f8518
supundileepa00 <paranagamasupun@gmail.com>
2023-03-19 22:52:43 +0530

    feat : Added routes to server file

--- EcoExplorer/backend/server.js
+++ EcoExplorer/backend/server.js
@@ -9,6 +9,8 @@ import usersRoutes from "./routes/users.routes.js";
 import questionAnswersRoutes from "./routes/questionAnswers.routes.js";
 import marksRoutes from "./routes/marks.routes.js";
 import infoRoutes from "./routes/info.routes.js";
+import interestedRoutes from "./routes/interested.routes.js";
+import eventRoutes from "./routes/events.routes.js";
 
 dotenv.config();
 const app = express();
@@ -18,7 +20,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.get("/rest-api/v1/", (req, res) => {
-    res.json({ message: "Welcome to Eco Explorer Backend" });
+  res.json({ message: "Welcome to Eco Explorer Backend" });
 });
 
 // routes
@@ -26,21 +28,23 @@ app.use("/rest-api/v1/users", usersRoutes);
 app.use("/rest-api/v1/marks", marksRoutes);
 app.use("/rest-api/v1/question-answers", questionAnswersRoutes);
 app.use("/rest-api/v1/info", infoRoutes);
+app.use("/rest-api/v1/events", eventRoutes);
+app.use("/rest-api/v1/interested", interestedRoutes);
 
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.b6wbfao.mongodb.net/?retryWrites=true&w=majority`;
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        app.listen(PORT, () =>
-            console.log(`Server Running on port :http://localhost:${PORT}`)
-        );
-    })
-    .catch((error) => {
-        console.log(error.message);
-    });
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server Running on port :http://localhost:${PORT}`)
+    );
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
