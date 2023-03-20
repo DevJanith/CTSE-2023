4f4244cf2bfc5a6bb46eb3079476759cf7a9ed4c
janithGamage <janith.g@informaticsint.com>
2023-03-20 09:14:11 +0530

    fix: update desc : yarn lock API configuration file

--- EcoExplorer/frontend/aquaorg/api/index.js
+++ EcoExplorer/frontend/aquaorg/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const aquaOrgAPI = axios.create({
     //your IP address here
-    baseURL: "http://192.168.1.3:5000/rest-api/v1/"
+    baseURL: "http://192.168.1.3:8000/rest-api/v1/"
     // baseURL : "http://aqua-org.herokuapp.com/rest-api/v1/"
 })
 
